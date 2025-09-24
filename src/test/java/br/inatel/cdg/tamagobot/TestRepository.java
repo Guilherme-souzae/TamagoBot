0e53e6eab2baf5fadd33b7d194e43a296776422e
Guilherme-souzae <guiemergente@gmail.com>
2025-09-24 16:55:47 -0300

    test: teste repo

--- src/test/java/br/inatel/cdg/tamagobot/TestRepository.java
+++ src/test/java/br/inatel/cdg/tamagobot/TestRepository.java
@@ -1,6 +1,16 @@
 package br.inatel.cdg.tamagobot;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 public class TestRepository
 {
-
+    @Test
+    public void TestCreateSucess()
+    {
+        Repository repo = new Repository();
+        Entity entity = new Entity("123", "Minos Prime", "https://");
+        assertDoesNotThrow(() -> repo.create(entity));
+    }
 }
